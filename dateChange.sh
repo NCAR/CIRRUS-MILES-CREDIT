2148b26d84d72241176ee9af7b7bb0f874a6d78a
sgpearse <pearse@ucar.edu>
2025-10-15 11:56:17 -0600

    test

--- dateChange.sh
+++ dateChange.sh
@@ -21,15 +21,19 @@ esac
 #start_time=$(TZ="America/Denver" date +"%Y-%m-%d %H:00:00")
 #end_time=$(TZ="America/Denver" date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S")
 
-start_time=$(TZ="America/Denver" date +"%Y-%m-%d %H:00:00"); \
-end_time=$(TZ="America/Denver" date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S")
+#start_time=$(TZ="America/Denver" date +"%Y-%m-%d %H:00:00"); \
+#end_time=$(TZ="America/Denver" date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S")
 
 #TZ="America/Denver"
 #start_time=$(date +"%Y-%m-%d %H:00:00")
 #end_time=$(date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S")
 
-echo "start time $start_time"
-echo "end   time $end_time"
+#echo "start time $start_time"
+#echo "end   time $end_time"
+
+start_time=$(TZ="America/Denver" date +"%Y-%m-%d %H:00:00"); \
+end_time=$(TZ="America/Denver" date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S"); \
+echo "start=$start_time end=$end_time"
 
 # Apply sed replacements in-place
 sed -i \
