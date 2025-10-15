f7d7343b5aeedee921ad4b6733e05a74d3f14ee9
sgpearse <pearse@ucar.edu>
2025-10-15 08:22:08 -0600

    test

--- credit.sh
+++ credit.sh
@@ -4,7 +4,7 @@ cp -r /glade/campaign/cisl/vast/pearse/wxformer_1h/finetune_final /checkpoint
 cp /glade/campaign/cisl/vast/pearse/save_loc_dynamic_forcing/solar_irradiance_2025-01-01_0000_2025-12-31_2300.nc /checkpoint
 export PATH=/home/ubuntu/.local/bin:$PATH
 cd /workspace/CIRRUS-MILES-CREDIT
-git pull
+git pull -q
 #chmod 777 ./credit.sh
 echo "GFS INIT!!!!"
 rm -rf /output/*
