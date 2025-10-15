09b69d48496d8719def2c1dbebd7ae6012ebb0f5
sgpearse <pearse@ucar.edu>
2025-10-15 11:26:52 -0600

    test

--- dateChange.sh
+++ dateChange.sh
@@ -13,9 +13,10 @@ case $hour in
 esac
 
 # Build timestamps
+TZ="America/Denver"
 date_str=$(date +%Y-%m-%d)
 start_time="${date_str} $(printf "%02d" $start_hour):00:00"
-end_time=$(TZ="America/Denver" date -d "$start_time America/Denver +6 hours" +"%Y-%m-%d %H:%M:%S")
+end_time=$(TZ="America/Denver" date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S")
 
 echo "start time $start_time"
 echo "end   time $end_time"
