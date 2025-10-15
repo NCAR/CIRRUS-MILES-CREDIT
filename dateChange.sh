fea3cebef565fdd093afbec5222ddc90f8fb0fb3
sgpearse <pearse@ucar.edu>
2025-10-15 11:20:23 -0600

    test

--- dateChange.sh
+++ dateChange.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Path to the file to edit
 FILE="/workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml"
-export TZ="America/Denver"
+#export TZ="America/Denver"
 
 # Determine which 6-hour window we're in
 hour=$(date +%H)
@@ -15,7 +15,7 @@ esac
 # Build timestamps
 date_str=$(date +%Y-%m-%d)
 start_time="${date_str} $(printf "%02d" $start_hour):00:00"
-end_time=$(date -d "$start_time America/Denver +6 hours" +"%Y-%m-%d %H:%M:%S")
+end_time=$(TZ="America/Denver" date -d "$start_time America/Denver +6 hours" +"%Y-%m-%d %H:%M:%S")
 
 echo "start time $start_time"
 echo "end   time $end_time"
