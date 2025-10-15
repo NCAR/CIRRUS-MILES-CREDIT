7cc887cc590040c98176d31543c87eb19a2caf93
sgpearse <pearse@ucar.edu>
2025-10-15 07:40:38 -0600

    test

--- dateChange.sh
+++ dateChange.sh
@@ -2,6 +2,8 @@
 # Path to the file to edit
 FILE="/workspace/CIRRUS-MILES-CREDIT/model_predict_old.yaml"
 
+echo "changing date on $FILE"
+
 # Determine which 6-hour window we're in
 hour=$(date +%H)
 case $hour in
@@ -16,9 +18,14 @@ date_str=$(date +%Y-%m-%d)
 start_time="${date_str} $(printf "%02d" $start_hour):00:00"
 end_time=$(date -d "$start_time +6 hours" +"%Y-%m-%d %H:%M:%S")
 
+echo start and end
+echo $start_time
+echo $end_time
+
 # Apply sed replacements in-place
 sed -i \
   -e "s|^\(.*forecast_start_time: *\).*|\1\"${start_time}\"|" \
   -e "s|^\(.*forecast_end_time: *\).*|\1\"${end_time}\"|" \
   "$FILE"
 
+cat $FILE
