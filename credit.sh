4b3c2fe3f7b421c55f8e8750a4d61da937e73d89
sgpearse <pearse@ucar.edu>
2025-10-21 09:45:30 -0600

    Test o21

--- credit.sh
+++ credit.sh
@@ -2,6 +2,7 @@
 nvidia-smi
 ls /glade/campaign/cisl/vast/pearse/wxformer_1h/finetune_final
 cp -r /glade/campaign/cisl/vast/pearse/wxformer_1h/finetune_final /checkpoint
+ls -lrth /checkpoint
 cp /glade/campaign/cisl/vast/pearse/save_loc_dynamic_forcing/solar_irradiance_2025-01-01_0000_2025-12-31_2300.nc /checkpoint
 echo "CHECKPOINT"
 ls /checkpoint
