7105d8ecc0c91c4f2366ec1774c0e9d895d7c880
sgpearse <pearse@ucar.edu>
2025-10-22 15:39:54 -0600

    test

--- credit.sh
+++ credit.sh
@@ -2,6 +2,7 @@
 nvidia-smi
 ls /glade/campaign/cisl/vast/pearse/credit_arxiv_models/wxformer_1h/finetune_final
 export PATH=/home/ubuntu/.local/bin:$PATH
+git -C /workspace/miles-credit stash
 git -C /workspace/miles-credit pull -q
 git -C /workspace/miles-credit branch
 echo "GFS INIT!!!!"
