cf13733bdf2972b633d9adbba37143807599548a
sgpearse <pearse@ucar.edu>
2025-10-22 07:32:46 -0600

    test

--- credit.sh
+++ credit.sh
@@ -1,27 +1,13 @@
 #! /bin/bash
 nvidia-smi
 ls /glade/campaign/cisl/vast/pearse/credit_arxiv_models/wxformer_1h/finetune_final
-#mkdir -p /checkpoint/finetune_final
-#cp -rv /glade/campaign/cisl/vast/pearse/wxformer_1h/finetune_final/* /checkpoint/finetune_final
-#cp /glade/campaign/cisl/vast/pearse/save_loc_dynamic_forcing/solar_irradiance_2025-01-01_0000_2025-12-31_2300.nc /checkpoint
-#echo "CHECKPOINT"
-#ls /checkpoint
-#echo "CHECKPOINT/FINETUNE_FINAL"
-#ls /checkpoint/finetune_final
 export PATH=/home/ubuntu/.local/bin:$PATH
-#cd /workspace/CIRRUS-MILES-CREDIT
-#cd /workspace/miles-credit
-#git pull -q
 git -C /workspace/miles-credit pull -q
-#chmod 777 ./credit.sh
+git -C /workspace/miles-credit branch
 echo "GFS INIT!!!!"
-#rm -rf /output/*
 conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
 mkdir -p /output/wxformer_1h_gfs
 echo "ROLLOUT REALTIME"
-#ls -lrth /checkpoint
 cp /workspace/CIRRUS-MILES-CREDIT/output.py /workspace/miles-credit/credit
-#conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
-pwd
 conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
 ls -lrth /output
