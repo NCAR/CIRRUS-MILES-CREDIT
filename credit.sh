63e8fd2caea8577071ac9015641a7524f6ce1423
sgpearse <pearse@ucar.edu>
2025-10-23 07:45:49 -0600

    test

--- credit.sh
+++ credit.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 #nvidia-smi
-#ls /glade/campaign/cisl/vast/pearse/credit_arxiv_models/wxformer_1h/finetune_final
 export PATH=/home/ubuntu/.local/bin:$PATH
 git -C /workspace/miles-credit stash
 git -C /workspace/miles-credit pull -q
+echo "miles-credit commit:"
 git -C /workspace/miles-credit log -1
-echo "GFS INIT!!!!"
+echo "gfs_init.py!"
 conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
 #mkdir -p /output/wxformer_1h_gfs
-echo "ROLLOUT REALTIME"
+echo "rollout_realtime.py!"
 conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
 ls -lrth /output
