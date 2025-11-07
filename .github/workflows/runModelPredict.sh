01376446eaa0b79619e65b1e3fda1c057f2899e6
sgpearse <pearse@ucar.edu>
2025-11-07 08:49:48 -0700

    test

---
+++ .github/workflows/runModelPredict.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#nvidia-smi
+export PATH=/home/ubuntu/.local/bin:$PATH
+git -C /workspace/miles-credit stash
+git -C /workspace/miles-credit pull -q
+echo "miles-credit commit:"
+git -C /workspace/miles-credit log -1
+echo "gfs_init.py!"
+conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
+#mkdir -p /output/model_predict
+echo "rollout_realtime.py!"
+conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
+ls -lrth /output
