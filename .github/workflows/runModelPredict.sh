2722ac50299fe2e7c24b35ee6b4b7692658028c5
sgpearse <pearse@ucar.edu>
2025-11-11 11:44:07 -0700

    test

--- .github/workflows/runModelPredict.sh
+++ .github/workflows/runModelPredict.sh
@@ -6,7 +6,7 @@ echo "miles-credit commit:"
 #git -C /workspace/miles-credit log -1
 echo "gfs_init.py!"
 #conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
-conda run -n credit python -u /_w/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
+conda run -n credit python -u /__w/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
 echo "rollout_realtime.py!"
 #conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
 conda run -n credit python -u /__w/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
