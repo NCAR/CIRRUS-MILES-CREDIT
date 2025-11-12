a2ba97b7df77a1d454751653cb92b640edf1db3e
sgpearse <pearse@ucar.edu>
2025-11-12 11:49:04 -0700

    test

--- credit.sh
+++ credit.sh
@@ -6,8 +6,8 @@ export PATH=/home/ubuntu/.local/bin:$PATH
 echo "miles-credit commit:"
 git -C /workspace/miles-credit log -1 --format=%H
 echo "gfs_init.py!"
-conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
-#mkdir -p /output/model_predict
+#conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
+conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c ./model_predict_old.yml
 echo "rollout_realtime.py!"
 conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
 ls -lrth /output
