54711fd3c97efdb0b9ee0b332bda5cc4cf8c8862
sgpearse <pearse@ucar.edu>
2026-01-28 11:29:09 -0700

    delete all model_output directories except the 4 most recent ones

--- credit.sh
+++ credit.sh
@@ -6,9 +6,10 @@ git -C /workspace/miles-credit log -1 --format=%H
 echo "gfs_init.py!"
 # Create /output/model_predict in case it does not exist
 mkdir -p /output/model_predict
+ls -lrth /output
 # Remove all directories but the 4 most recently created outputs
 ls -1dt /output/model_predict/* | tail -n +5 | xargs -r rm -rf
-pm -rf /output/gfs_init*.zarr
+rm -rf /output/gfs_init*.zarr
 conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c ./model_predict_old.yml
 echo "rollout_realtime.py!"
 conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
