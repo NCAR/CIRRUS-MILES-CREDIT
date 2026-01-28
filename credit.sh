de6554cb47a3e17a89b903328c496596a0515d82
sgpearse <pearse@ucar.edu>
2026-01-28 11:19:33 -0700

    delete all model_output directories except the 4 most recent ones

--- credit.sh
+++ credit.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 #nvidia-smi
 export PATH=/home/ubuntu/.local/bin:$PATH
-#git -C /workspace/miles-credit stash
-#git -C /workspace/miles-credit pull -q
 echo "miles-credit commit:"
 git -C /workspace/miles-credit log -1 --format=%H
 echo "gfs_init.py!"
-#conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c /workspace/CIRRUS-MILES-CREDIT/model_predict_old.yml
-rm -rf /output/gfs_init*.zarr
+# Remove all directories but the 4 most recently created outputs
+ls -1dt /output/model_predict/* | tail -n +5 | xargs -r rm -rf
+pm -rf /output/gfs_init*.zarr
 conda run -n credit python -u /workspace/miles-credit/applications/gfs_init.py -c ./model_predict_old.yml
 echo "rollout_realtime.py!"
 conda run -n credit python -u /workspace/miles-credit/applications/rollout_realtime.py -c ./model_predict_old.yml
