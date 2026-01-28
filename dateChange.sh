074d072633f7814fa5c3df172a9a250b312b64ff
sgpearse <pearse@ucar.edu>
2026-01-28 09:26:40 -0700

    change timezone to GMT

--- dateChange.sh
+++ dateChange.sh
@@ -19,24 +19,24 @@ fi
 #hour=$(TZ="$TZ" date +%H)
 hour=$(date +%H)
 case $hour in
-  00|01|02|03|04|05) start_hour=00 ;;
-  06|07|08|09|10|11) start_hour=06 ;;
-  12|13|14|15|16|17) start_hour=12 ;;
-  *) start_hour=18 ;;
+  00|01|02|03|04|05) end_hour=00 ;;
+  06|07|08|09|10|11) end_hour=06 ;;
+  12|13|14|15|16|17) end_hour=12 ;;
+  *) end_hour=18 ;;
 esac
 
 #date_str=$(TZ="$TZ" date +%Y-%m-%d)
 date_str=$(date +%Y-%m-%d)
-start_time="${date_str} $(printf "%02d" $start_hour):00:00"
-echo "start time 1 $start_time"
+end_time="${date_str} $(printf "%02d" $end_hour):00:00"
+echo "start time 1 $end_time"
 
 # Get epoch time for now in Denver
 #epoch_start=$(TZ=$TZ date --date="$start_time" +%s)
-epoch_start=$(date --date="$start_time" +%s)
+epoch_end=$(date --date="$end_time" +%s)
 #epoch_end=$(TZ=$TZ date --date="$start_time" +%s)
 
 # Add 6 hours (in seconds)
-epoch_end=$((epoch_start + 6*3600))
+epoch_start=$((epoch_start - 6*3600))
 #epoch_start=$((epoch_end - 6*3600))
 
 echo "epoch_start $epoch_start"
